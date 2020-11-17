0b31e2fa59ca573e540e885e23ffed57d68e66b3
fernando duenas <fernando39615@aim.com>
2020-11-17 14:54:17 -0500

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const readline = require('readline');
+const data = require('./data.js')
 
 
 const interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 
 const menu = `
 Your options are:
@@ -18,16 +20,53 @@ Your options are:
 
 `;
 
-const handleMenu = function(cmd) {
-  if (cmd === '6') {
-    console.log('Quitting!')
-    interface.close();
-  } else {
-    console.clear();
-    console.log('Type 6 to quit!');
-    interface.question(menu, handleMenu);
+
+const handleMenu = cmd => {
+  switch(cmd){
+    case '1':
+      interface.question("What should go on your todo list? ", add);
+      break;
+
+    case '2':
+      console.log("This feature is under construction, check back later!");
+      interface.question(menu, handleMenu);
+      break;
+
+    case '3':
+      console.log("This feature is under construction, check back later!");
+      interface.question(menu, handleMenu);
+      break;
+
+    case '4':
+      console.log("This feature is under construction, check back later!");
+      interface.question(menu, handleMenu);
+      break;
+
+    case '5':
+      console.log("This feature is under construction, check back later!");
+      interface.question(menu, handleMenu);
+      break;
+
+    case '6':
+      console.log('Quitting!')
+      interface.close();
+      interface.question(menu, handleMenu);
+      break;
   }
 };
 
+const add = adding => {
+  console.log('* ' + adding);
+  for(const toDo of data.todos){
+    console.log('* ' + toDo.text);
+  }
+  interface.question(menu, handleMenu)
+}
+
 console.clear();
+
+for(const toDo of data.todos){
+  console.log('* ' + toDo.text);
+}
+
 interface.question(menu, handleMenu);
